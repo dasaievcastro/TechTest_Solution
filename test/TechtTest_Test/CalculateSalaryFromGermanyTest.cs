5713cb1384303a09a4bdd3aecf5f7440359a967b
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 10:20:53 +0100

    Red Step - Salary from Germany not implemented

---
+++ test/TechtTest_Test/CalculateSalaryFromGermanyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TechTest_Domain.Builders.Employees;
+using Xunit;
+
+namespace TechtTest_Test
+{
+    public class CalculateSalaryFromGermanyTest
+    {
+        public CalculateSalaryFromGermanyTest()
+        {
+        }
+
+        [Fact]
+        /*The gross amount is the result after multiplying HoursWorked and HourRate*/
+        public void Should_calculate_the_gross_amount_if_employee_has_hourworked_and_hourRate()
+        {
+            var HourWorked = 60;
+            var HourRate = 10;
+            var GermanyEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromGermany calculate = new CalculateSalaryFromGermany(GermanyEmployee);
+            var GrossAmountTemplate = HourRate * HourWorked;
+            Assert.Equal(calculate.GrossAmount, GrossAmountTemplate);
+        }
+    }
+}
