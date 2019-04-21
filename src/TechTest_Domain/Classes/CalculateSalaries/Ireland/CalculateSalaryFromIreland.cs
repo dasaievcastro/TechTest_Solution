a62e71fa7b692050811c4324d239025122d5625d
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 09:30:43 +0100

    Green and refactoring step - Implement tax rate

--- src/TechTest_Domain/Classes/CalculateSalaries/Ireland/CalculateSalaryFromIreland.cs
+++ src/TechTest_Domain/Classes/CalculateSalaries/Ireland/CalculateSalaryFromIreland.cs
@@ -6,12 +6,16 @@ namespace TechTest_Domain.Classes.CalculateSalaries.Ireland
     public class CalculateSalaryFromIreland
     {
         public double GrossAmount { get; protected set; }
+        public double TaxRateAmount { get; protected set; }
 
         public CalculateSalaryFromIreland(Employee e)
         {
             this.GrossAmount = e.HourRate * e.HoursWorked;
         }
 
-
+        public void CalculateTaxRate()
+        {
+            TaxRateAmount = (GrossAmount > 600) ? GrossAmount * .4 : GrossAmount * .25;
+        }
     }
 }
