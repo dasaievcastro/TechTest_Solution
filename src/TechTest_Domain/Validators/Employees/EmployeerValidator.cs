cef6cd72490586e94ae988bf014edb51ff607631
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 09:17:06 +0100

    Implement Validation and Refactoring

--- src/TechTest_Domain/Validators/Employees/EmployeerValidator.cs
+++ src/TechTest_Domain/Validators/Employees/EmployeerValidator.cs
@@ -13,10 +13,20 @@ namespace TechTest_Domain.Validators.Employees
             .NotEmpty().WithMessage("Please enter a Hour Worked")
             .Must(BeAValideHourWorked).WithMessage("Please enter a valid Hour Worked");
 
+            RuleFor(p => p.HourRate)
+            .Cascade(CascadeMode.StopOnFirstFailure)
+            .NotEmpty().WithMessage("Please enter a Hour Rate")
+            .Must(BeAValideHourRate).WithMessage("Please enter a valid Hour Rate");
         }
+
         public bool BeAValideHourWorked(int _hourWorked)
         {
             return (_hourWorked > 0);
+        }
+
+        public bool BeAValideHourRate(int _hourRate)
+        {
+            return (_hourRate > 0);
         }
     }
 }
