1d76d5987ee8e107e1e69c2c41da0e07ad0583fe
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 09:36:31 +0100

    Red step - Pension not implemented

--- test/TechtTest_Test/CalculateSalaryFromIrelandTest.cs
+++ test/TechtTest_Test/CalculateSalaryFromIrelandTest.cs
@@ -62,5 +62,31 @@ namespace TechtTest_Test
             var SocialTaxAmountTemplate = HourRate * HourWorked * .07;
             Assert.Equal(calculate.SocialTaxAmount, SocialTaxAmountTemplate);
         }
+
+        [Fact]
+        /*a Universal social charge of 7% is applied for the first €500 euro and 8% thereafter*/
+        public void Should_calculate_the_social_contribuation_tax_of_8_percent_if_the_grossamount_is_greater_than_500_euro()
+        {
+            var HourWorked = 51;
+            var HourRate = 10;
+            var IrelandEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromIreland calculate = new CalculateSalaryFromIreland(IrelandEmployee);
+            calculate.CalculateSocialContribution();
+            var SocialTaxAmountTemplate = HourRate * HourWorked * .08;
+            Assert.Equal(calculate.SocialTaxAmount, SocialTaxAmountTemplate);
+        }
+
+        [Fact]
+        /*a compulsory pension contribution of 4% is applied*/
+        public void Should_calculate_the_pension_tax_of_4_percent()
+        {
+            var HourWorked = 60;
+            var HourRate = 10;
+            var IrelandEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromIreland calculate = new CalculateSalaryFromIreland(IrelandEmployee);
+            calculate.CalculatePension();
+            var PensionAmountTemplate = HourRate * HourWorked * .04;
+            Assert.Equal(calculate.PensionAmount, PensionAmountTemplate);
+        }
     }
 }
