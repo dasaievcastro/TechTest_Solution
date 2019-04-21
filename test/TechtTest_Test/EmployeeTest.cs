5fd09144dd06cfafb7c02d4fe8a5d6790196384b
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 08:35:42 +0100

    Create Employee Test

---
+++ test/TechtTest_Test/EmployeeTest.cs
@@ -0,0 +1,29 @@
+using System;
+using ExpectedObjects;
+using Xunit;
+
+namespace TechtTest_Test
+{
+    public class EmployeeTest
+    {
+        public EmployeeTest()
+        {
+
+        }
+
+        [Fact]
+        public void Should_have_create_a_employee()
+        {
+            var EmployeeTemplate = new
+            {
+                HoursWorked = 10,
+                HourRate = 40,
+                Location = 1
+
+            };
+            var EmployeeObject = new Employee(10, 40, 1);
+            //I make a simple compare just to check if the EmployeeTemplate are equal to Employee.
+            EmployeeTemplate.ToExpectedObject().ShouldMatch(EmployeeObject);
+        }
+    }
+}
