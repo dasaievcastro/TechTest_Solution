bc969228d0649e52b166669ec6c018a36034da85
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 10:14:15 +0100

    Red step - Italy Social tax not implemented

--- test/TechtTest_Test/CalculateSalaryFromItalyTest.cs
+++ test/TechtTest_Test/CalculateSalaryFromItalyTest.cs
@@ -35,5 +35,33 @@ namespace TechtTest_Test
             var TaxeRateAmountTemplate = HourRate * HourWorked * .25;
             Assert.Equal(calculate.TaxRateAmount, TaxeRateAmountTemplate);
         }
+
+        [Fact]
+        /*This is charged at 9% for the first €500 and increases by .1% over every €100 thereafter.*/
+        public void Should_calculate_the_social_contribution_tax_of_9_percent_if_the_grossamount_is_less_than_500_euro()
+        {
+            var HourWorked = 50;
+            var HourRate = 10;
+            var ItalyEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromItaly calculate = new CalculateSalaryFromItaly(ItalyEmployee);
+            calculate.CalculateSocialContribution();
+            var SocialTaxAmountTemplate = HourRate * HourWorked * .09;
+            Assert.Equal(calculate.SocialTaxAmount, SocialTaxAmountTemplate);
+        }
+
+        [Fact]
+        /*This is charged at 9% for the first €500 and increases by .1% over every €100 thereafter.*/
+        public void Should_calculate_the_social_contribution_tax_of_9_plus_x_percent_if_the_grossamount_is_greater_than_500_euro()
+        {
+            var HourWorked = 60;
+            var HourRate = 10;
+            var ItalyEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromItaly calculate = new CalculateSalaryFromItaly(ItalyEmployee);
+            calculate.CalculateSocialContribution();
+            var GrossAmountTemplate = HourRate * HourWorked;
+            float ExtraTax = (GrossAmountTemplate - 500) / 100;
+            var SocialTaxAmountTemplate = Math.Round(GrossAmountTemplate * (.09 + (ExtraTax / 100)));
+            Assert.Equal(calculate.SocialTaxAmount, SocialTaxAmountTemplate);
+        }
     }
 }
