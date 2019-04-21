2a1c300b6a36a52dc2b179e743570c815db04fe6
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 09:22:43 +0100

    Red Step - CalculateSalaryFromIreland doesnt exist

---
+++ test/TechtTest_Test/CalculateSalaryFromIrelandTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TechTest_Domain.Builders.Employees;
+using Xunit;
+
+namespace TechtTest_Test
+{
+    public class CalculateSalaryFromIrelandTest
+    {
+        public CalculateSalaryFromIrelandTest()
+        {
+        }
+
+        [Fact]
+        /*The gross amount is the result after multiplying HoursWorked and HourRate*/
+        public void Should_calculate_the_gross_amount_if_employee_has_hourworked_and_hourRate()
+        {
+            var HourWorked = 60;
+            var HourRate = 10;
+            var IrelandEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromIreland calculate = new CalculateSalaryFromIreland(IrelandEmployee);
+            var GrossAmountTemplate = HourRate * HourWorked;
+            Assert.Equal(calculate.GrossAmount, GrossAmountTemplate);
+        }
+    }
+}
