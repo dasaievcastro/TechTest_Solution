32a7fc8e909d1529baa63445ef02e33de5268cc3
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-21 10:09:53 +0100

    Green and refactoring steps - Italy salary class

---
+++ src/TechTest_Domain/Classes/CalculateSalaries/Italy/CalculateSalaryFromItaly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TechTest_Domain.Classes.CalculateSalaries.Base;
+using TechTest_Domain.Classes.Employees;
+
+namespace TechTest_Domain.Classes.CalculateSalaries.Italy
+{
+    public class CalculateSalaryFromItaly : CalculateSalaryBase
+    {
+        public CalculateSalaryFromItaly(Employee e) : base(e)
+        {
+        }
+    }
+}
--- test/TechtTest_Test/CalculateSalaryFromItalyTest.cs
+++ test/TechtTest_Test/CalculateSalaryFromItalyTest.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using TechTest_Domain.Builders.Employees;
+using TechTest_Domain.Classes.CalculateSalaries.Italy;
+using Xunit;
+
 namespace TechtTest_Test
 {
     public class CalculateSalaryFromItalyTest
     {
         public CalculateSalaryFromItalyTest()
         {
+        }
+
+        [Fact]
+        /*The gross amount is the result after multiplying HoursWorked and HourRate*/
+        public void Should_calculate_the_gross_amount_if_employee_has_hourworked_and_hourRate()
+        {
+            var HourWorked = 60;
+            var HourRate = 10;
+            var ItalyEmployee = BuilderEmployee.Create().EarnsForHour(HourRate).WorkedFor(HourWorked).Build();
+            CalculateSalaryFromItaly calculate = new CalculateSalaryFromItaly(ItalyEmployee);
+            var GrossAmountTemplate = HourRate * HourWorked;
+            Assert.Equal(calculate.GrossAmount, GrossAmountTemplate);
         }
     }
 }
